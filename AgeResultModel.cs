1e9165db65f2da58a6038d107c2f489654d87a1c
Никита Черных <ispeed@MacBook-Air-Nikita.local>
2026-09-11 09:08:38 +0300

    Remove duplicated root-level ASP files

--- AgeResultModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MyApi;
-
-public sealed class AgeResultModel
-{
-    public string UserName { get; init; } = string.Empty;
-    public int Age { get; init; }
-    public bool IsAdult => Age >= 18;
-}
